cc9541cd2276825e7276de3ea04f6fca15d21b69
Andreu <andreuhuguet@gmail.com>
2024-01-03 14:11:47 +0100

    llargs

--- src/components/extra/Header.js
+++ src/components/extra/Header.js
@@ -1,7 +1,7 @@
 function Header() {
     return (
         <header>
-            <h1>Transcripció automàtica d'àudios</h1>
+            <h1>Transcripció automàtica d'àudios (llargs!)</h1>
             <p id="warning-desktop">Aquesta aplicació requereix Google Chrome i anar per ordinador per funcionar correctament.</p>
         </header>
     );
