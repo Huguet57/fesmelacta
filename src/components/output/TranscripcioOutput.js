98146f643ccf20cf91d1c839d8ad2bac0820c4f7
Andreu <andreuhuguet@gmail.com>
2024-01-03 12:53:06 +0100

    check icon

--- src/components/output/TranscripcioOutput.js
+++ src/components/output/TranscripcioOutput.js
@@ -4,6 +4,16 @@ const CopyIcon = () => (
     <svg width="12" height="12" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="icon-sm"><path fill-rule="evenodd" clip-rule="evenodd" d="M12 4C10.8954 4 10 4.89543 10 6H14C14 4.89543 13.1046 4 12 4ZM8.53513 4C9.22675 2.8044 10.5194 2 12 2C13.4806 2 14.7733 2.8044 15.4649 4H17C18.6569 4 20 5.34315 20 7V19C20 20.6569 18.6569 22 17 22H7C5.34315 22 4 20.6569 4 19V7C4 5.34315 5.34315 4 7 4H8.53513ZM8 6H7C6.44772 6 6 6.44772 6 7V19C6 19.5523 6.44772 20 7 20H17C17.5523 20 18 19.5523 18 19V7C18 6.44772 17.5523 6 17 6H16C16 7.10457 15.1046 8 14 8H10C8.89543 8 8 7.10457 8 6Z" fill="currentColor"></path></svg>
 );
 
+const CheckIcon = () => (
+    <svg width="12" height="12" xmlns="http://www.w3.org/2000/svg" version="1.0" viewBox="0 0 1171.000000 1280.000000" preserveAspectRatio="xMidYMid meet">
+        <metadata>
+        Created by potrace 1.15, written by Peter Selinger 2001-2017
+        </metadata>
+        <g transform="translate(0.000000,1280.000000) scale(0.100000,-0.100000)" fill="#000000" stroke="none">
+        <path d="M11555 12694 c-1288 -888 -2591 -2076 -3945 -3594 -1475 -1656 -3026 -3783 -4315 -5918 -72 -119 -115 -180 -123 -177 -8 3 -716 474 -1575 1048 -859 574 -1568 1047 -1576 1052 -11 6 -10 2 2 -16 98 -140 3704 -5078 3709 -5079 3 0 34 66 68 148 1225 2918 2422 5234 3838 7427 1148 1777 2481 3497 3899 5028 91 97 163 177 161 177 -2 0 -67 -43 -143 -96z"/>
+        </g>
+    </svg>
+);
 
 function TranscripcioOutput({ lines }) {
     const [filterBrackets, setFilterBrackets] = useState(false);
@@ -61,7 +71,7 @@ function TranscripcioOutput({ lines }) {
                     onClick={handleCopyClick}
                 >
                     {
-                        copySuccess ? 'Copiat!' : <><CopyIcon /> Copia el text</>
+                        copySuccess ? <><CheckIcon /> Copiat!</> : <><CopyIcon /> Copia el text</>
                     }
                 </button>
                 <div style={{ display: 'flex', alignItems: 'center', fontSize: '12px' }}>
