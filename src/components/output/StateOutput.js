14a145b667ba508d0910a048a2ca0c5afa4f751c
Andreu <andreuhuguet@gmail.com>
2024-01-03 19:07:49 +0100

    state on placeholder

--- src/components/output/StateOutput.js
+++ src/components/output/StateOutput.js
@@ -1,27 +1,27 @@
-function StateOutput({ state}) {
-    const printState = (state) => {
-        switch (state) {
-            case 0:
-                return 'Falta triar quin tipus de transcripció vols fer i carregar un àudio';
-            case 1:
-                return 'Falta carregar un àudio';
-            case 2:
-                return 'Falta triar quin tipus de transcripció vols fer';
-            case 3:
-                return 'Preparat.';
-            case 4:
-                return 'Processant àudio...';
-            case 5:
-                return 'Àudio processat. Comença la transcripció...';
-            case 6:
-                return 'Transcripció en curs...';
-            case 7:
-                return 'Transcripció finalitzada';
-            default:
-                return 'Desconegut';
-        }
+export const printState = (state) => {
+    switch (state) {
+        case 0:
+            return 'Falta triar quin tipus de transcripció vols fer i carregar un àudio';
+        case 1:
+            return 'Falta carregar un àudio';
+        case 2:
+            return 'Falta triar quin tipus de transcripció vols fer';
+        case 3:
+            return 'Preparat.';
+        case 4:
+            return 'Processant àudio...';
+        case 5:
+            return 'Àudio processat. Comença la transcripció...';
+        case 6:
+            return 'Transcripció en curs...';
+        case 7:
+            return 'Transcripció finalitzada';
+        default:
+            return 'Desconegut';
     }
+}
 
+function StateOutput({ state}) {
     return (
         <div>
             <div
