53dcbc7bb64671d3444b2f0e51fd0a7c7d4582cc
Andreu <andreuhuguet@gmail.com>
2024-01-31 20:54:06 +0100

    punt

--- src/components/extra/Credits.js
+++ src/components/extra/Credits.js
@@ -3,7 +3,7 @@ import '../../styles/credits.css';
 function Credits() {
     return (
         <div className="credits">
-            <span>Fet amb <a href="https://github.com/ggerganov/whisper.cpp/tree/master/examples/whisper.wasm">whisper.cpp</a> i <a href="https://github.com/FL33TW00D/whisper-turbo/tree/master">whisper-turbo</a></span>
+            <span>Fet amb <a href="https://github.com/ggerganov/whisper.cpp/tree/master/examples/whisper.wasm">whisper.cpp</a> i <a href="https://github.com/FL33TW00D/whisper-turbo/tree/master">whisper-turbo</a>.</span>
             <span>Fet per <a href="https://x.com/HuguetAndreu/status/1729269729607717369?s=20">Andreu Huguet</a>. Preguntes o dubtes obra'm a <a href="https://twitter.com/HuguetAndreu">Twitter</a>.</span>
         </div>
     )
