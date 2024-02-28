8f5b6471552a2771d4b7900efec5104ca8fb1827
Andreu <andreuhuguet@gmail.com>
2024-02-28 16:53:37 +0100

    role alert

--- src/components/extra/Header.js
+++ src/components/extra/Header.js
@@ -2,7 +2,7 @@ function Header() {
     return (
         <header>
             <h1>Transcripció automàtica en català</h1>
-            <p id="warning-desktop">Aquesta aplicació requereix Google Chrome i anar per ordinador per funcionar correctament.</p>
+            <p id="warning-desktop" role="alert">Aquesta aplicació requereix Google Chrome i anar per ordinador per funcionar correctament.</p>
         </header>
     );
 }
